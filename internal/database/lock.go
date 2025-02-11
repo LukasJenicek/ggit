73634c094f330a6b0e831076ccb6ede3252c514d
Lukas Jenicek <lukas.jenicek5@gmail.com>
2025-02-11 16:30:05 +0100

    better err messages

--- internal/database/lock.go
+++ internal/database/lock.go
@@ -12,20 +12,20 @@ func (*AtomicFileWriter) Update(path string, content string) error {
 
 	f, err := os.OpenFile(lockPath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0644)
 	if err != nil {
-		return fmt.Errorf("opening file %q: %w", path, err)
+		return fmt.Errorf("creating temp file %q: %w", lockPath, err)
 	}
 	defer f.Close()
 
 	if _, err := f.WriteString(content); err != nil {
-		return fmt.Errorf("writing to file %q: %w", lockPath, err)
+		return fmt.Errorf("writing to temp file %q: %w", lockPath, err)
 	}
 
 	if err := f.Sync(); err != nil {
-		return fmt.Errorf("sync to file %q: %w", lockPath, err)
+		return fmt.Errorf("sync to temp file %q: %w", lockPath, err)
 	}
 
 	if err = os.Rename(lockPath, path); err != nil {
-		return fmt.Errorf("rename tmp object %s: %w", lockPath, err)
+		return fmt.Errorf("rename temp file %s: %w", lockPath, err)
 	}
 
 	return nil
