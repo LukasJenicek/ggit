fe7ac096365cb930d2de343dce6cc80a098dcde2
Lukas Jenicek <lukas.jenicek5@gmail.com>
2025-02-24 20:37:30 +0100

    use filepath.Join to make path construct work on windows

--- internal/workspace/workspace_test.go
+++ internal/workspace/workspace_test.go
@@ -30,13 +30,13 @@ func TestWorkspace_ListFiles(t *testing.T) {
 
 	expectedFiles := []*workspace.File{
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/a/a.txt",
+			Path: filepath.Join(testDataFolder, "a", "a.txt"),
 		},
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/a.txt",
+			Path: filepath.Join(testDataFolder, "a.txt"),
 		},
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/b/b.txt",
+			Path: filepath.Join(testDataFolder, "b", "b.txt"),
 		},
 	}
 
@@ -62,13 +62,42 @@ func TestWorkspace_ListSpecificFiles(t *testing.T) {
 
 	expectedFiles := []*workspace.File{
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/a/a.txt",
+			Path: filepath.Join(testDataFolder, "a", "a.txt"),
 		},
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/a.txt",
+			Path: filepath.Join(testDataFolder, "a.txt"),
 		},
 		{
-			Path: "/home/lj/Projects/LukasJenicek/ggit/testdata/b/b.txt",
+			Path: filepath.Join(testDataFolder, "b", "b.txt"),
+		},
+	}
+
+	assert.EqualValues(t, expectedFiles, files)
+}
+
+func TestWorkspace_ListSpecificFile(t *testing.T) {
+	t.Parallel()
+
+	projectRootFolder, err := helpers.GetProjectRootFolder()
+	if err != nil {
+		t.Error(err)
+	}
+
+	testDataFolder := filepath.Join(projectRootFolder, "testdata")
+
+	w := workspace.New(testDataFolder, filesystem.New())
+
+	files, err := w.ListFiles("a.txt")
+	if err != nil {
+		t.Errorf("list files: %s", err)
+	}
+
+	expectedFiles := []*workspace.File{
+		{
+			Path: filepath.Join(testDataFolder, "a", "a.txt"),
+		},
+		{
+			Path: filepath.Join(testDataFolder, "a.txt"),
 		},
 	}
 
